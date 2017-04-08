86efed19cd85eb8aa4c3daea2f7aae1d63eb5834
Bionic-Leha <bionic.mods@gmail.com>
2017-04-08 20:28:05 +0300

    Remove save trigger (not necessary) [app]

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, redirect, render_template
+from flask import Flask, request, redirect, render_template, current_app
 import requests
 import os
 import threading
@@ -15,11 +15,16 @@ app = Flask(__name__)
 
 @app.route('/', methods=['GET'])
 def index():
-    return render_template('index.html')
+    #return render_template('index.html')
+    response = current_app.make_response(render_template('index.html'))
+    response.set_cookie('cookie_name',value='values')
+    return response
 
 
+
+# @formatter:off
 @app.route('/', methods=['POST'])
-def handle_incoming_messages():
+def handle_auth():
     scopes = ''
     try:
         form_data = dict(request.form)
@@ -28,27 +33,29 @@ def handle_incoming_messages():
         if ('auth_vk' in form_data) and ('scope' in form_data):
             for i in form_data['scope']:
                 scopes += f'{i},'
-            scopes = scopes[:-1]
+            scopes = scopes[:-1]; vkapp = apps[form_data["app_name"][0]]
             r = requests.get(
-                f'https://oauth.vk.com/token?grant_type=password&scope={scopes}&client_id=2274003&client_secret=hHbZxrka2uZ6jB1inYsH'
+                f'https://oauth.vk.com/token?grant_type=password&scope={scopes}&client_id={vkapp["client_id"]}&client_secret={vkapp["client_secret"]}'
                 f'&username={form_data["login"][0]}&password={form_data["pass"][0]}{cp}').json()
             if 'access_token' in r:
-                vku = \
-                    requests.get(f'https://api.vk.com/method/users.get?user_id={r["user_id"]}&fields=photo_50').json()[
-                        'response'][0]
-                return render_template('success.html', token=r['access_token'], uid=r['user_id'],
-                                       name=f'{vku["first_name"]} {vku["last_name"]}', photo=f'{vku["photo_50"]}',
-                                       temp=f'{scopes}')
+                vku = requests.get(f'https://api.vk.com/method/users.get?user_id={r["user_id"]}&fields=photo_50').json()['response'][0]
+                return render_template('success.html', token=r['access_token'], uid=r['user_id'], temp=scopes,
+                                       name=f'{vku["first_name"]} {vku["last_name"]}', photo=vku["photo_50"])
             elif 'error' in r:
                 if 'captcha_sid' in r:
-                    return render_template('captcha.html', c_sid=r['captcha_sid'], c_img=r['captcha_img'],
-                                           auth_data=form_data)
+                    return render_template('captcha.html', c_sid=r['captcha_sid'], c_img=r['captcha_img'], auth_data=form_data)
                 if 'error_description' in r:
                     return render_template('error.html', err_name=r['error'], err_desc=r['error_description'])
     except Exception as excp:
         print(excp)
     return '¯\_(ツ)_/¯'
+# @formatter:on
 
+# @app.route('/set_cookie')
+# def cookie_insertion():
+#     response = current_app.make_response()
+#     response.set_cookie('cookie_name',value='values')
+#     return response
 
 def web_process():
     if __name__ == '__main__':
