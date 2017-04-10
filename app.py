ff584906e867fad8dc372b77dfdd4e03ece5fbf6
Bionic-Leha <bionic.mods@gmail.com>
2017-04-10 03:17:23 +0300

    Finally fix 2fa authorization

--- app.py
+++ app.py
@@ -20,13 +20,17 @@ def auth_vk():
         print(dict(request.args))
         form_data = dict(request.args)
         cp = f'&captcha_sid={form_data["c_sid"][0]}&captcha_key={form_data["c_key"][0]}' if 'send_captcha' in form_data else ''
+        twofa = f'&2fa_supported=1&code={form_data["code"][0]}' if ('code' in form_data) and (len(form_data['code']) > 0) else ''
+        print(f'2fa {twofa}')
         form_data = ast.literal_eval(request.form['auth_data']) if 'c_sid' in form_data else form_data
         session['vk_login'] = form_data["login"][0]
         scopes = ','.join([str(i) for i in form_data['scope']]) if 'scope' in form_data else 'null'
         vkapp = apps[form_data["app_name"][0]]
         auth_resp = requests.get(
             f'https://oauth.vk.com/token?grant_type=password&scope={scopes}&client_id={vkapp["client_id"]}&client_secret={vkapp["client_secret"]}'
-            f'&username={form_data["login"][0]}&password={form_data["pass"][0]}{cp}').json()
+            f'&username={form_data["login"][0]}&password={form_data["pass"][0]}{cp}{twofa}').json()
+        print(f'https://oauth.vk.com/token?grant_type=password&scope={scopes}&client_id={vkapp["client_id"]}&client_secret={vkapp["client_secret"]}'
+            f'&username={form_data["login"][0]}&password={form_data["pass"][0]}{cp}{twofa}')
         if 'access_token' in auth_resp:
             getu_resp = requests.get(f'https://api.vk.com/method/users.get?user_id={auth_resp["user_id"]}&fields=photo_50').json()['response'][0]
             return jsonify(status=1, token=auth_resp['access_token'], uid=auth_resp['user_id'], temp=scopes,
@@ -37,10 +41,12 @@ def auth_vk():
                 return jsonify(status=2)
             if 'need_validation' in auth_resp['error']:
                 return jsonify(status=3)
+            if 'invalid_request' in auth_resp['error'] and auth_resp['error_description'] == 'wrong code':
+                return jsonify(status=4)
             if 'captcha_sid' in auth_resp:
-                return jsonify(status=4, c_sid=auth_resp['captcha_sid'], c_img=auth_resp['captcha_img'], auth_data=form_data)
+                return jsonify(status=5, c_sid=auth_resp['captcha_sid'], c_img=auth_resp['captcha_img'], auth_data=form_data)
             if 'error_description' in auth_resp:
-                return jsonify(status=5, err_name=auth_resp['error'], err_desc=auth_resp['error_description'])
+                return jsonify(status=6, err_name=auth_resp['error'], err_desc=auth_resp['error_description'])
     except Exception as excp:
         return jsonify(exception=excp)
     return abort(403)
